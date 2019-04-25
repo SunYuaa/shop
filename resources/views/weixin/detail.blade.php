6b15bcf384cac35a5372d000657052d86fbd4e90
SunYuaa <2861164631@qq.com>
2019-04-25 10:44:49 +0000

    bug

--- resources/views/weixin/detail.blade.php
+++ resources/views/weixin/detail.blade.php
@@ -38,8 +38,8 @@
             wx.updateAppMessageShareData({
                 title: '最新商品', // 分享标题
                 desc: 'Apple系列', // 分享描述
-                link: 'https://1809sunyujuan.comcto.com/goods/share', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                imgUrl: 'https://1809sunyujuan.comcto.com/image/share.jpeg', // 分享图标
+                link: 'http://1809sunyujuan.comcto.com/goods/share', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://1809sunyujuan.comcto.com/image/share.jpeg', // 分享图标
                 success: function () {
 //                alert('分享成功');
                 }
@@ -48,8 +48,8 @@
             wx.onMenuShareAppMessage({
                 title: '最新商品', // 分享标题
                 desc: 'Apple系列', // 分享描述
-                link: 'https://1809sunyujuan.comcto.com/goods/share', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                imgUrl: 'https://1809sunyujuan.comcto.com/image/share.jpeg', // 分享图标
+                link: 'http://1809sunyujuan.comcto.com/goods/share', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://1809sunyujuan.comcto.com/image/share.jpeg', // 分享图标
                 type: '', // 分享类型,music、video或link，不填默认为link
                 dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
                 success: function () {
