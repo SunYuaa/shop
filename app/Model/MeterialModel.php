6dbdc007c7ab8ed4d97d128cbbe1ae6a48ab3305
SunYuaa <2861164631@qq.com>
2019-04-26 10:38:45 +0000

    素材

---
+++ app/Model/MeterialModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MeterialModel extends Model
+{
+    //
+    protected $table = 'wx_material';
+    public $primaryKey = false;
+}
